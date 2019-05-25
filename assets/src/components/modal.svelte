7d24f08b4f0cf49ef4fd8b6a12333ec75884d3b7
wskurniawan <wisnu.kurniawan@mail.ugm.ac.id>
2019-05-25 14:59:18 +0700

    add ui

---
+++ assets/src/components/modal.svelte
@@ -0,0 +1,21 @@
+{#if show}
+<div class="ws-modal-container">
+   <div class="uk-height-viewport uk-flex uk-flex-middle uk-flex-center">
+      <div class="uk-align-center uk-width-2-5@m uk-width-1-1@s">
+         <div class="uk-card uk-card-default">
+            <div class="uk-card-header">
+               <button class=" uk-button uk-button-link uk-text-danger uk-align-right" on:click={() => show = false}>close</button>
+            </div>
+            <div class="uk-card-body">
+               <slot></slot>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+{/if}
+
+<script>
+   import { createEventDispatcher } from 'svelte';
+   export var show;
+</script>
